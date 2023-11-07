8dd1dad40922bce3e102c77a113a70b142cbb6da
00012467 <wiut12467@gmail.com>
2023-11-07 08:54:35 +0500

    Finished deploy part

---
+++ Bash Scripts/windowsinstance.sh
@@ -0,0 +1,3 @@
+wget -Uri https://aws-codedeploy-us-east-1.s3.amazonaws.com/latest/codedeploy-agent.msi -OutFile "C:\Users\Administrator\Downloads\ codedeploy-agent.msi"
+
+https://dotnet.microsoft.com/en-us/download/dotnet/7.0
--- SQL Queries/CreateScript.sql
+++ SQL Queries/CreateScript.sql
@@ -1,15 +1,4 @@
-﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
-BEGIN
-    CREATE TABLE [__EFMigrationsHistory] (
-        [MigrationId] nvarchar(150) NOT NULL,
-        [ProductVersion] nvarchar(32) NOT NULL,
-        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
-    );
-END;
-GO
-
-BEGIN TRANSACTION;
-GO
+﻿GO
 
 CREATE TABLE [Genres] (
     [GenreId] uniqueidentifier NOT NULL,
@@ -31,22 +20,5 @@ CREATE TABLE [Games] (
 GO
 
 CREATE INDEX [IX_Games_GameGenreGenreId] ON [Games] ([GameGenreGenreId]);
-GO
-
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20231025152638_InitialCreate', N'7.0.13');
-GO
-
-COMMIT;
-GO
-
-BEGIN TRANSACTION;
-GO
-
-INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
-VALUES (N'20231028091534_MigrationToAws', N'7.0.13');
-GO
-
-COMMIT;
 GO
 
